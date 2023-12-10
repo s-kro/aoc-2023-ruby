f0eba0bde051ec7c285f66c09566c6319d4619f8
Stephen Krochenski <skrochen@krotek.com>
2023-12-10 10:06:50 -0500

    edit comments

--- aoc_2023-7b.rb
+++ aoc_2023-7b.rb
@@ -6,14 +6,14 @@ CARD_RANK = %w[A K Q T 9 8 7 6 5 4 3 2 J]
 
 game = Array.new
 File.readlines('aoc_2023-7.dat').each do |hb| # group into same cards,
-  hand, bid = hb.split(' ') # ... join the same cards together, then sort the 'of a kinds' to first by size
+  hand, bid = hb.split(' ') # ... join like cards together, then sort the 'of a kinds' to first pos by no of cards
   sh = hand.chars.sort_by{|a| CARD_RANK.find_index(a.to_s)}.chunk{|c|c}.map{|n, a| a.join}.sort_by{|a| -a.length}
   (0..sh.length - 1).each do |i|
     if (sh[i].chars.first == 'J' && sh.length > 1) then # check against 5 jokers, no sense doing anything
       i == 0 ? sh[1].concat(sh[i]) : sh[0].concat(sh[i]) # ... as they're already concatinated
       sh.delete_at(i)                                # need to break because now the array length 
-      break                                          #  ... no longer matches the array    
-    end
+      break                                          #  ... no longer matches the array (avoid looping 
+    end                                              # ... though elements that no longer exist)
   end
   game.push [sh, hand.chars, bid]
 end
